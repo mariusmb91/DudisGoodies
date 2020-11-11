1ca30b4d8cdbb36d6e74cf106d469fa73781fb06
Marius Baltaretu <57147159+mariusmb91@users.noreply.github.com>
2020-11-11 23:52:30 +0200

    main-content page added and facebook and instagram buttons fixed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainContentComponent } from './main-content/main-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainContentComponent
   ],
   imports: [
     BrowserModule
